f3d9a0278a165705017fec4537c32bde09796088
Jihee Ryu <chrisjihee@etri.re.kr>
2023-06-23 15:00:28 +0900

    * Update setup environment script: python=3.10 * Save outputs:   - NER: 91.87 F1c, 84.81 F1e   - DP:  92.94 UAS, 87.22 LAS

--- klue_baseline/__init__.py
+++ klue_baseline/__init__.py
@@ -1,5 +1,5 @@
 """ klue_baseline package info """
-__version__ = "1.6.0.dev0"
+__version__ = "1.6.1.dev0"
 __author__ = "KLUE project contributors"
 
 
--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda create -n KLUE-baseline python=3.9 -y
+conda create -n KLUE-baseline python=3.10 -y
 conda activate KLUE-baseline
 pip install -r requirements.txt
 conda list
