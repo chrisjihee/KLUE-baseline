8d1e42b42ef22a160364c7cd30740578c68b38e6
Jihee Ryu <chrisjihee@etri.re.kr>
2023-07-13 15:29:45 +0900

    Update scripts

--- run_2.sh
+++ run_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python run_klue.py train --task klue-ner --output_dir output2 --data_dir data/klue-ner --num_train_epochs 10 --max_seq_length 510 --metric_key character_macro_f1 --accelerator gpu --devices 2 --model_name_or_path pretrained-pro/ETRI-RoBERTa-Base-bbpe2303
-python run_klue.py train --task klue-dp --output_dir output2 --data_dir data/klue-dp --num_train_epochs 20 --max_seq_length 256 --metric_key las_macro_f1 --warmup_ratio 0.1 --accelerator gpu --devices 2 --model_name_or_path pretrained-pro/ETRI-RoBERTa-Base-bbpe2303
+python run_klue.py train --task klue-ner --output_dir output2/run_2 --data_dir data/klue-ner --num_train_epochs 10 --max_seq_length 510 --metric_key character_macro_f1 --accelerator gpu --devices 2 --model_name_or_path pretrained-pro/ETRI-RoBERTa-Base-bbpe2303
+python run_klue.py train --task klue-dp --output_dir output2/run_2 --data_dir data/klue-dp --num_train_epochs 20 --max_seq_length 256 --metric_key las_macro_f1 --warmup_ratio 0.1 --accelerator gpu --devices 2 --model_name_or_path pretrained-pro/ETRI-RoBERTa-Base-bbpe2303
